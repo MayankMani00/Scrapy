66fc5d2375cc6bb57fabb92bb47da786cdce0714
MayankMani00 <43845197+MayankMani00@users.noreply.github.com>
2019-05-28 04:43:19 +0530

    Update CB_pro.py

--- CB_pro.py
+++ CB_pro.py
@@ -31,8 +31,8 @@ class MySpider(scrapy.Spider):
 				"price" : price,
 			}
 		final="https://www.flipkart.com/mobiles/pr?sid=tyy%2C4io&p%5B%5D=facets.processor_brand%255B%255D%3DSnapdragon&p%5B%5D=facets.serviceability%5B%5D%3Dfalse&p%5B%5D=facets.offer_type%255B%255D%3DExchange%2BOffer&otracker=clp_banner_1_10.bannerX3.BANNER_mobile-phones-store_HPUGCU9BYBF6&fm=neo%2Fmerchandising&iid=M_934db066-154e-4074-a4b1-96f56a0af28e_6.HPUGCU9BYBF6&ppt=HomePage&ppn=Home&ssid=85m4yqvgzk0000001558978084715&page=6"
-		next_page_id=response.css('a.._1ypTIJ._3fVaIS::attr(href)').get()
-			
+		next_page_id=response.css("nav._1ypTIJ a._3fVaIS::attr(href)").get()
+		#^this statement is where I'm facing the issue	
 		if next_page_id is not final: 
 			next_page=response.urljoin(next_page_id)
 			yield scrapy.Request(next_page,callback=self.parse)	
